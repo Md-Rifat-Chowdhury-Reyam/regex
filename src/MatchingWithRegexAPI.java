784a8612cd07e3f21d42d547e588784d6a07fe16
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-30 22:33:12 +0600

    regex pattern match

---
+++ src/MatchingWithRegexAPI.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+
+public class MatchingWithRegexAPI {
+
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches("Rifat", "Tafir"));
+
+        System.out.println(Pattern.matches("Reya*m", "Reyaam"));
+        System.out.println(Pattern.matches(
+                "geeksforge*ks", "geeksforgeeks"));
+    }
+}
