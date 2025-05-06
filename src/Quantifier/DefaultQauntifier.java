262ef8f69548334e1370b113cc14d65b640f2c3d
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-06 16:29:41 +0600

    Quantifier Greedy Qualifiers, Reluctant Quantifier ,Possessive Quantifiers

--- src/Quantifier/DefaultQauntifier.java
+++ src/Quantifier/DefaultQauntifier.java
@@ -11,34 +11,58 @@ public class DefaultQauntifier {
 
         Matcher m = p.matcher("Rifat");
 
-        while (m.find())
-        {
-            System.out.println("Pattern found " + m.start() + " to " + (m.end()-1));
+        while (m.find()) {
+            System.out.println("Pattern found " + m.start() + " to " + (m.end() - 1));
         }
 
+        System.out.println();
 
         //Reluctant Quantifier (Appending ? after a quantifier)
         Pattern p1 = Pattern.compile("R+?"); // Making an instance of Pattern class
-                                                // Here "+" is a Reluctant quantifier because
-                                                // a "?' is appended after it.
-        Matcher m1 = p1.matcher("Rifat");
+        // Here "+" is a Reluctant quantifier because
+        // a "?' is appended after it.
+        Matcher m1 = p1.matcher("RRfRat");
 
-        while (m1.find())
-        {
-            System.out.println("Pattern found " + m1.start() + " to " + (m1.end()-1));
+        while (m1.find()) {
+            System.out.println("Pattern found " + m1.start() + " to " + (m1.end() - 1));
         }
 
+        System.out.println();
 
         //Possessive Quantifier (Appending + after a quantifier)
         Pattern p3 = Pattern.compile("R++"); // Making an instance of Pattern class
-                                                        // Here "+" is a Possessive quantifier because
-                                                        // a "+' is appended after it.
+        // Here "+" is a Possessive quantifier because
+        // a "+' is appended after it.
 
         Matcher m3 = p3.matcher("Rifat");
-        while (m3.find())
-        {
-            System.out.println("Pattern found " + m3.start() + " to " + (m3.end()-1));
+        while (m3.find()) {
+            System.out.println("Pattern found " + m3.start() + " to " + (m3.end() - 1));
         }
 
+        System.out.println();
+
+
+        // Create a pattern with Greedy quantifier
+        Pattern pg = Pattern.compile("g+g");
+
+        // Create same pattern with possessive quantifier
+        Pattern pp = Pattern.compile("g++g");
+
+        System.out.println("Using Greedy Quantifier");
+        Matcher mg = pg.matcher("ggg");
+
+        while (mg.find()) {
+            System.out.println("Pattern found from " + mg.start() +
+                    " to " + (mg.end() - 1));
+        }
+
+        System.out.println("\nUsing Possessive Quantifier");
+        Matcher mp = pp.matcher("ggg");
+
+        while (mp.find()) {
+            System.out.println("Pattern found from " + mp.start() +
+                    " to " + (mp.end() - 1));
+
+        }
     }
 }
