160da120517b2df430e13f410359958db05fa74e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-06 13:17:05 +0600

    character class regex

--- src/Character/Example1.java
+++ src/Character/Example1.java
@@ -1,18 +1,12 @@
 package Character;
 
-public class Example1 {
-
-
-
-    public static void checkValid( int element)
-    {
-        if(element == 2)
-        {
-            System.out.println("Valid character");
-        }
-        else
-        {
-            System.out.println("Invalid character");
+public class Example1
+{
+    public static void checkValid(int charCount) {
+        if (charCount == 1 || charCount == 2) {
+            System.out.println("Valid character (charCount: " + charCount + ")");
+        } else {
+            System.out.println("Invalid character (charCount: " + charCount + ")");
         }
     }
 
@@ -20,7 +14,9 @@ public class Example1 {
 
         //charCount(argchar)
         int argchar1 =  0x9999; //argchar is parameter present Unicode point of character to be count
-        int argchar2 =  0x10000;
+                                //Returns 1 for BMP characters (U+0000 to U+FFFF)
+
+        int argchar2 =  0x10000; //Returns 2 for supplementary characters (U+10000 to U+10FFFF)
         int argchar3 =  0x10001;
 
         int c1 = Character.charCount(argchar1); //charCount() method used
@@ -28,15 +24,18 @@ public class Example1 {
         int c3 = Character.charCount(argchar3);
 
         //pass to chechValid Method to varify validation;
-        checkValid(c1);
-        checkValid(c2);
+        checkValid(c1); // Will print "Invalid character" (1 != 2)
+        checkValid(c2); // Will print "Valid character" (2 == 2)
         checkValid(c3);
 
+        System.out.println();
 
         //charValue()
         Character m = new Character('r'); // create a Character object and assign a char value;
         char ch = m.charValue(); //object m value assign to a char variable using charValue() method;
         System.out.println("Primitive value " + ch);
+
+        System.out.println();
 
         //codePointAt():
         char[] arguments = new char[] {'r', 'i' ,'f' , 'a', 't'};
@@ -44,18 +43,8 @@ public class Example1 {
         int val2;
         val1 = Character.codePointAt(arguments, 3); //returns Unicode Point of the character array present at the argumented position.
         val2 = Character.codePointAt(arguments, 0);
-        System.out.println("Unicode code point at : " + arguments[0] + " = " + val1);
-        System.out.println("Unicode code point at : " + arguments[4] + " = " + val2);
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Unicode code point at : " + arguments[3] + " = " + val1);
+        System.out.println("Unicode code point at : " + arguments[0] + " = " + val2);
 
     }
 
